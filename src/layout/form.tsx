3cbbdbbeba0242b040c2facd56295ed0773cb701
Negar Sarani <negar.sarani2000@gmail.com>
2023-05-14 10:24:35 +0330

    components

---
+++ src/layout/form.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const  Form = () => {
+    return (
+        <div>
+            <h1>
+                Setup Quiz
+            </h1>
+        </div>
+    );
+};
+
+export default Form;
